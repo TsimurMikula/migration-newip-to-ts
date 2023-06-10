79c5e5c86465832f88f2dd5dd2c2d2df2864e4c9
Tsimur Mikula <mikulatsimur@yandex.ru>
2023-06-10 16:39:59 +0300

    feat: adding an API key

--- migration-to-ts/src/components/controller/appLoader.js
+++ migration-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'd4009ad3bf9e4afa9db01c104ef3bd0e', // получите свой ключ https://newsapi.org/
         });
     }
 }
