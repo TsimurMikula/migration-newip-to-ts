d45ed87edbe023eb036187e0232cb2bbfa67d92d
Tsimur Mikula <mikulatsimur@yandex.ru>
2023-06-12 23:23:29 +0300

    chore: replacing the proxy for the deployment

--- migration-to-ts/src/components/controller/appLoader.ts
+++ migration-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://rss-news-api.onrender.com/', {
             apiKey: 'd4009ad3bf9e4afa9db01c104ef3bd0e', // получите свой ключ https://newsapi.org/
         });
     }
